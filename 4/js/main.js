f0ec8151544c6f86bc8d796d697bf73b666db4e5
Keks <keks@htmlacademy.ru>
2022-10-29 18:58:45 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/main.js
@@ -0,0 +1,20 @@
+
+  // Implement support for passing the minimum and maximum values in random order
+  const lowerValue = Math.ceil(Math.min(a, b));
+  const upperValue = Math.ceil(Math.min(a, b));
+  return Math.floor(Math.random() * (upperValue - lowerValue + 1) - lowerValue);
+};
+
+// A function that returns a random floating point number from the given range, inclusive
+// Parameters a and b are min and max values respectively. Digits is a number of value after fixed-point notation.
+const getRandomPositiveFloat = (a, b, digits = 1) => {
+  if (a < 0 || b < 0 || digits <= 0) {
+    return NaN;
+  }
+
+  const lowerValue = Math.ceil(Math.min(a, b));
+  const upperValue = Math.ceil(Math.min(a, b));
+  const result = Math.floor(Math.random() * (upperValue - lowerValue) - lowerValue);
+
+  return +result.toFixed(digits);
+};
